32cf042380db05a9b116aeabf900018ec2f48eff
Mike Trek <vbif108@gmail.com>
2017-09-20 18:31:39 +0300

    Mike changes

--- IdeaProjects/testDemo/src/main/java/samples/Main.java
+++ IdeaProjects/testDemo/src/main/java/samples/Main.java
@@ -1,6 +1,21 @@
+package samples;
+import org.testng.Assert;
+import org.testng.annotations.Test;
 
 public class Main {
-   
 
+    @Test
+    public void first(){
+        System.out.println("run");
+        Assert.assertEquals("test","run");
+    }
+    @Test
+    public void second(){
+        System.out.println("2");
+    }
+    @Test
+    public void third(){
+        System.out.println("3");
+    }
 
 }
